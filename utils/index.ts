773b47774f10bc3d13933d2573613851fcfede33
ntnyq <ntnyq13@gmail.com>
2024-03-30 16:06:40 +0800

    chore: cleanup moot deps

--- utils/index.ts
+++ utils/index.ts
@@ -8,5 +8,3 @@
  * @returns Promise that resolves after the given time
  */
 export const waitFor = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
-export * from './validatePrettierOptions'
