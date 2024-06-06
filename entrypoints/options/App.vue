06ea3ec2399435c1f375b10c8293f0f1d6375a25
ntnyq <ntnyq13@gmail.com>
2024-06-07 03:58:19 +0800

    fix: fix responsive layout

--- entrypoints/options/App.vue
+++ entrypoints/options/App.vue
@@ -7,7 +7,7 @@ useConfigStore()
 </script>
 
 <template>
-  <main class="h-screen flex flex-col">
+  <main class="h-screen min-w-[720px] flex flex-col">
     <Navbar class="h-$h-header" />
     <RouterView class="h-[calc(100vh-var(--h-header))]" />
   </main>
