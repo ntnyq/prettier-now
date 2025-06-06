2179657cfcee882d47b496c0ebab3b823f7ea83d
ntnyq <ntnyq13@gmail.com>
2025-06-06 13:20:19 +0800

    chore: update

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,4 +1,4 @@
 import path from 'node:path'
 
-export const resolve = (...args: string[]) =>
+export const resolve = (...args: string[]): string =>
   path.resolve(import.meta.dirname, '..', ...args)
