dcdeb927a197b257c0fba603818f9cdaa9b64281
ntnyq <ntnyq13@gmail.com>
2025-12-26 15:57:49 +0800

    fix: prevent blink from toggle dark

--- composables/dark.ts
+++ composables/dark.ts
@@ -46,6 +46,7 @@ export function toggleDark(event?: MouseEvent) {
       {
         duration: 400,
         easing: 'ease-in',
+        fill: 'forwards',
         pseudoElement: isDark.value
           ? '::view-transition-old(root)'
           : '::view-transition-new(root)',
