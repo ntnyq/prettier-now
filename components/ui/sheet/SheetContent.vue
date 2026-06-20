273fab2dd3ed8b767d60a37458ff8ab4a6e4495f
ntnyq <ntnyq13@gmail.com>
2026-06-20 10:22:32 +0800

    fix: mark sheet close slot

--- components/ui/sheet/SheetContent.vue
+++ components/ui/sheet/SheetContent.vue
@@ -57,6 +57,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 
       <DialogClose
         class="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none"
+        data-slot="sheet-close"
       >
         <X class="size-4" />
         <span class="sr-only">Close</span>
